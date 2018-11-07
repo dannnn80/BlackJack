bd7027db0b0ca3f23d869314358d4b5418ee5c62
Xcode User <xcode@fasts-macbook-pro-2.local>
2018-11-06 23:53:12 -0500

    Initial Commit

---
+++ BlackJack/Users.swift
@@ -0,0 +1,23 @@
+//
+//  Users.swift
+//  BlackJack
+//
+//  Created by Xcode User on 2018-11-05.
+//  Copyright © 2018 Xcode User. All rights reserved.
+//
+
+import Foundation
+public class Users {
+    public var Fullname: String = ""
+    public var Username: String = ""
+    public var Password: String = ""
+    //public var Wins: Int?
+    //public var Losses: Int?
+  //  public var currChipAmt : Int?
+    
+    
+  //  public init(fullname)
+    
+    
+    
+}
